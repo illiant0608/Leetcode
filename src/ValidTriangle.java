81bc61d73fe09bf0d48b7ddc901face923cad074
illiant <effortblesses@outlook.com>
2018-09-08 20:50:02 +0800

    可以先对数组排序降低复杂度

---
+++ src/ValidTriangle.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class ValidTriangle {
+    public int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int count = 0;
+        int n = nums.length;
+
+        for (int i = n - 1;i >= 2;i--) {
+            int l = 0, r = i - 1;
+            while (l < r) {
+                if (nums[l] + nums[r] > nums[i]){
+                    count += r-l;
+                    r--;
+                }else
+                    l++;
+            }
+        }
+
+        return count;
+    }
+    
+}
